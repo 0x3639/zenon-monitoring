57d77a4d76d0aa80d12e66a8f18c8a9035720921
0x3639 <5406130+0x3639@users.noreply.github.com>
2022-11-28 04:48:50 -0600

    Update monitoring-installer.sh

--- grafana/monitoring-installer.sh
+++ grafana/monitoring-installer.sh
@@ -20,10 +20,10 @@ usage () {
   echo "   --1      Step 1: Installs Prometheus"
   echo "   --2      Step 2: Installs Grafana"
   echo "   --3      Step 3: Installs node_exporter"
-  echo "   --4      Step 4: Installs AvalancheGo Grafana dashboards"
+  echo "   --4      Step 4: Installs Go-Zenono Grafana dashboards"
   echo "   --5      Step 5: (Optional) Installs additional dashboards"
   echo ""
-  echo "Run without any options, script will download and install latest version of AvalancheGo dashboards."
+  echo "Run without any options, script will download and install latest version of Go-Zenon dashboards."
 }
 
 #helper function to check for presence of required commands, and install if missing
@@ -67,14 +67,14 @@ get_environment() {
 }
 
 install_prometheus() {
-  echo "AvalancheGo monitoring installer"
+  echo "Go-Zenon monitoring installer"
   echo "--------------------------------"
   echo "STEP 1: Installing Prometheus"
   echo
   get_environment
   check_reqs
-  mkdir -p /tmp/avalanche-monitoring-installer/prometheus
-  cd /tmp/avalanche-monitoring-installer/prometheus
+  mkdir -p /tmp/gozenon-monitoring-installer/prometheus
+  cd /tmp/gozenon-monitoring-installer/prometheus
 
   promFileName="$(curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep -o "http.*linux-$getArch\.tar\.gz")"
   if [[ $(wget -S --spider "$promFileName"  2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
@@ -145,7 +145,7 @@ install_prometheus() {
 }
 
 install_grafana() {
-  echo "AvalancheGo monitoring installer"
+  echo "Go-Zenon monitoring installer"
   echo "--------------------------------"
   echo "STEP 2: Installing Grafana"
   echo
@@ -177,18 +177,18 @@ install_grafana() {
 }
 
 install_exporter() {
-  echo "AvalancheGo monitoring installer"
+  echo "Go-Zenon monitoring installer"
   echo "--------------------------------"
   echo "STEP 3: Installing node_exporter"
   echo
   get_environment
-  mkdir -p /tmp/avalanche-monitoring-installer/exporter_archive
-  cd /tmp/avalanche-monitoring-installer/exporter_archive
+  mkdir -p /tmp/gozenon-monitoring-installer/exporter_archive
+  cd /tmp/gozenon-monitoring-installer/exporter_archive
   echo "Dowloading archive..."
   nodeFileName="$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep -o "http.*linux-$getArch\.tar\.gz")"
   echo $nodeFileName
   wget -nv --show-progress -O node_exporter.tar.gz "$nodeFileName"
-  tar xvf node_exporter.tar.gz -C /tmp/avalanche-monitoring-installer/exporter_archive --strip-components=1
+  tar xvf node_exporter.tar.gz -C /tmp/gozenon-monitoring-installer/exporter_archive --strip-components=1
   sudo mv /tmp/avalanche-monitoring-installer/exporter_archive/node_exporter /usr/local/bin
   echo "Installed, version:"
   node_exporter --version
@@ -242,11 +242,11 @@ install_exporter() {
 
   cp /etc/prometheus/prometheus.yml .
   {
-    echo "  - job_name: 'avalanchego'"
+    echo "  - job_name: 'gozenon'"
     echo "    metrics_path: '/ext/metrics'"
     echo "    static_configs:"
     echo "      - targets: ['localhost:9650']"
-    echo "  - job_name: 'avalanchego-machine'"
+    echo "  - job_name: 'gozenon-machine'"
     echo "    static_configs:"
     echo "      - targets: ['localhost:9100']"
     echo "        labels:"
@@ -270,13 +270,13 @@ install_exporter() {
 install_dashboards() {
   #check for installation
   if test -f "/etc/grafana/grafana.ini"; then
-    echo "AvalancheGo monitoring installer"
+    echo "GoZenon monitoring installer"
     echo "--------------------------------"
   else
     echo "Node monitoring installation not found!"
     echo
     echo "Please refer to the tutorial:"
-    echo "https://docs.avax.network/nodes/maintain/setting-up-node-monitoring"
+    echo "[TO DO] https://docs.avax.network/nodes/maintain/setting-up-node-monitoring"
     echo
     usage
     exit 0
@@ -293,11 +293,11 @@ install_dashboards() {
 
   echo
   echo "Downloading..."
-  mkdir -p /tmp/avalanche-monitoring-installer/dashboards-install
-  cd /tmp/avalanche-monitoring-installer/dashboards-install
+  mkdir -p /tmp/gozenon-monitoring-installer/dashboards-install
+  cd /tmp/gozenon-monitoring-installer/dashboards-install
 
-  wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/c_chain.json
-  wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/database.json
+  # wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/c_chain.json
+  # wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/database.json
   wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/machine.json
   wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/main.json
   wget -nd -m -nv https://raw.githubusercontent.com/ava-labs/avalanche-monitoring/master/grafana/dashboards/network.json
